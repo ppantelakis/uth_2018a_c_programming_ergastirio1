861e5d9a7f99c48d39d18900591673b268ab5556
Panagiotis Pantelakis <panagiotis.pantelakis@gmail.com>
2018-04-17 22:15:34 +0300

    Lab0 project continue for age

--- lab0/lab0/main.c
+++ lab0/lab0/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//Global variable of type integer
+int age;
 int main()
 {
     printf("Hello world!\n");
+    printf("Please, type your age: ");
+    scanf("%d", &age);
+    printf("Your age is %d!\n", age);
+    printf("You were born in year %d\n", 2018-age);
     return 0;
 }
