fbf53dbe778e65496d0b29eb6a705229d9f58b2d
Panagiotis Pantelakis <panagiotis.pantelakis@gmail.com>
2018-05-04 14:14:10 +0300

    Project PanagiotisPantelakis_lab2.3 Printing the initialized board

--- lab2.3/PnagiotisPantelakis_lab2.3/main.c
+++ lab2.3/PnagiotisPantelakis_lab2.3/main.c
@@ -13,6 +13,7 @@ int main()
     int i,j;
     int curx=n_width/2,cury=n_height/2;//Current position of the player
     int trap[2][n_randtimes];//Trap coordinations
+    //Initialization of table
     for(i=0;i<n_width;i++)
     {
         for(j=0;j<n_height;j++)
@@ -20,13 +21,34 @@ int main()
             table[i][j]=' ';
         }
     }
+    //Initialization of player coordination
     table[curx][cury]='o';
+    //Adding in 4 random positions in table trap
     for(i=0;i<n_randtimes;i++)
     {
-        //Adding in 4 random positions in table trap
         //https://stackoverflow.com/questions/822323/how-to-generate-a-random-number-in-c
         trap[0][i]=rand() % n_width;
         trap[1][i]=rand() % n_height;
     }
+    //Printing the initialized table
+    for(i=0;i<n_width+2;i++)
+    {
+        printf("-");
+    }
+    printf("\n");
+    for(i=0;i<n_width;i++)
+    {
+        printf("|");
+        for(j=0;j<n_height;j++)
+        {
+            printf("%c ",table[i][j]);
+        }
+        printf("|\n");
+    }
+    for(i=0;i<n_width+2;i++)
+    {
+        printf("-");
+    }
+    printf("\n");
     return 0;
 }
