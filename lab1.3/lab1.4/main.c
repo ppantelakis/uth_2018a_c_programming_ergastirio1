401ae9f2d18f2815f10c52dd9e2972f0f8d3df08
Panagiotis Pantelakis <panagiotis.pantelakis@gmail.com>
2018-05-04 07:50:45 +0300

    Project Lab 1.4 added while and switch statement

--- lab1.3/lab1.4/main.c
+++ lab1.3/lab1.4/main.c
@@ -7,5 +7,36 @@ int main()
     int d, board=0, moves=0;
     srand((unsigned int) time(0));
     d = 1 + ((long long)6*rand())/(1+(long long)RAND_MAX);
+    while(board<=100)
+    {
+        moves++;
+        d = 1 + ((long long)6*rand())/(1+(long long)RAND_MAX);
+        switch(d)
+        {
+            case 1  :
+              board++;
+              break; /* optional */
+
+           case 2  :
+              board=board*2;
+              break; /* optional */
+
+            case 3  :
+              board=0;
+              break; /* optional */
+            case 4  :
+              break; /* optional */
+            case 5  :
+              board=board-2;
+              break; /* optional */
+            case 6  :
+              board=board+10;
+              break; /* optional */
+           /* you can have any number of case statements */
+           default : /* Not neseseray because 1<=d<=6 */
+                break;
+        }
+
+    }
     return 0;
 }
