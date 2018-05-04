f2b1b7cbf54055b29cec69cccf594dc31af04674
Panagiotis Pantelakis <panagiotis.pantelakis@gmail.com>
2018-05-04 14:07:41 +0300

    Project PanagiotisPantelakis_lab2.3 Adding const variable char x and filled trap table with random coordinates

--- lab2.3/PnagiotisPantelakis_lab2.3/main.c
+++ lab2.3/PnagiotisPantelakis_lab2.3/main.c
@@ -9,8 +9,10 @@
 int main()
 {
     char table[n_width][n_height];
+    const c_trap = 'x';//Trap character
     int i,j;
     int curx=n_width/2,cury=n_height/2;//Current position of the player
+    int trap[2][n_randtimes];//Trap coordinations
     for(i=0;i<n_width;i++)
     {
         for(j=0;j<n_height;j++)
@@ -21,9 +23,10 @@ int main()
     table[curx][cury]='o';
     for(i=0;i<n_randtimes;i++)
     {
-        //Adding in 4 random positions in table the char x
+        //Adding in 4 random positions in table trap
         //https://stackoverflow.com/questions/822323/how-to-generate-a-random-number-in-c
-         table[rand() % n_width][rand() % n_height] = 'x';
+        trap[0][i]=rand() % n_width;
+        trap[1][i]=rand() % n_height;
     }
     return 0;
 }
