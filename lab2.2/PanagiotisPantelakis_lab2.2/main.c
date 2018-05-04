fcf09483007826edbc564efd51d7ea7c48a1f2f8
Panagiotis Pantelakis <panagiotis.pantelakis@gmail.com>
2018-05-04 13:07:31 +0300

    Project PanagiotisPantelakis_lab2.2 finished the second sorting method

--- lab2.2/PanagiotisPantelakis_lab2.2/main.c
+++ lab2.2/PanagiotisPantelakis_lab2.2/main.c
@@ -5,17 +5,19 @@
 
 int main()
 {
-    int akeraioi[n_max], tablea[n_max], n, i, j, helpvar;//helpvar for switching variables
+    int akeraioi[n_max], tablea[n_max], tableb[n_max], n, i, j, helpvar, isswitched=1;//helpvar for switching variables, isswitched flag if switched variables
     for(n=0;n<n_max;n++)
     {
         printf("Give me the %d number : ",n+1);
         scanf("%d",&akeraioi[n]);
     }
-    //Copy the contents of tables akeraioi to tablea
+    //Copy the contents of tables akeraioi to tablea, tableb
     for(n=0;n<n_max;n++)
     {
         tablea[n] = akeraioi[n];
+        tableb[n] = akeraioi[n];
     }
+    //First sorting method
     for(i=n_max-1;i>0;i--)
     {
         for(j=0;j<=i;j++)
@@ -36,10 +38,42 @@ int main()
         printf("%d ",akeraioi[n]);
     }
     //Print the sorted array tablea
-    printf("\n\n The sorted array is \n\n");
+    printf("\n\n The sorted array table a is \n\n");
     for(n=0;n<n_max;n++)
     {
         printf("%d ",tablea[n]);
+    }
+
+    //Second sorting method
+    while(isswitched)
+    {
+        isswitched = 0;
+        for(i=0;i<n_max-1;i++)
+        {
+            if(tableb[i]>tableb[i+1])
+            {
+                //switch variables
+                helpvar = tableb[i];
+                tableb[i] = tableb[i+1];
+                tableb[i+1] = helpvar;
+                isswitched = 1;
+                break;
+            }
+        }
+
+    }
+
+    //Print the unsorted array akeraioi
+    printf("\n\n The unsorted array is\n\n");
+    for(n=0;n<n_max;n++)
+    {
+        printf("%d ",akeraioi[n]);
+    }
+    //Print the sorted array tablea
+    printf("\n\n The sorted array tableb is \n\n");
+    for(n=0;n<n_max;n++)
+    {
+        printf("%d ",tableb[n]);
     }
     return 0;
 }
