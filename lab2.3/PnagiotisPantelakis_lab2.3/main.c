7833c9a3c63ae73e8f55b9f0288c5e07420fa782
Panagiotis Pantelakis <panagiotis.pantelakis@gmail.com>
2018-05-04 13:47:09 +0300

    Project PanagiotisPantelakis_lab2.3 Create variable table,constants and initialization of table to space

--- lab2.3/PnagiotisPantelakis_lab2.3/main.c
+++ lab2.3/PnagiotisPantelakis_lab2.3/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define n_width 31
+#define n_height 41
+
+
 int main()
 {
+    char table[n_width][n_height];
+    int i,j;
+    for(i=0;i<n_width;i++)
+    {
+        for(j=0;j<n_height;j++)
+        {
+            table[i][j]=' ';
+        }
+    }
     printf("Hello world!\n");
     return 0;
 }
