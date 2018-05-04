241607722c711a1ef7f60ad479fefc1bf8a914fe
Panagiotis Pantelakis <panagiotis.pantelakis@gmail.com>
2018-05-04 13:58:46 +0300

    Project PanagiotisPantelakis_lab2.3 Adding in 4 random positions in table the char x

--- lab2.3/PnagiotisPantelakis_lab2.3/main.c
+++ lab2.3/PnagiotisPantelakis_lab2.3/main.c
@@ -3,6 +3,7 @@
 
 #define n_width 31
 #define n_height 41
+#define n_randtimes 4
 
 
 int main()
@@ -14,12 +15,18 @@ int main()
         for(j=0;j<n_height;j++)
         {
             table[i][j]=' ';
-            if(i==((n_width/2)+1) && j==((n_height/2)+1))
+            if(i==(n_width/2) && j==(n_height/2))
             {
-                table[i][j]='O';
+                table[i][j]='o';
                 printf("%d %d",i,j);
             }
         }
+    }
+    for(i=0;i<n_randtimes;i++)
+    {
+        //Adding in 4 random positions in table the char x
+        //https://stackoverflow.com/questions/822323/how-to-generate-a-random-number-in-c
+         table[rand() % n_width][rand() % n_height] = 'x';
     }
     return 0;
 }
