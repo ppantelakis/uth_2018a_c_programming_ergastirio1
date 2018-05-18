31211a1400a221c2a5a283dc21214aed3eb898a3
Panagiotis Pantelakis <panagiotis.pantelakis@gmail.com>
2018-05-18 11:44:51 +0300

    Finished exercise

--- lab3.1/PanagiotisPantelakis_lab3.1/main.c
+++ lab3.1/PanagiotisPantelakis_lab3.1/main.c
@@ -11,6 +11,9 @@
 #define POINTS_FILENAME "file31.txt"
 #define CHUNK 1024 /* read 1024 bytes at a time */
 
+//Table to keep x,fx,f'x
+double t_vals[3][TOTALPOINTS];
+
 FILE * file_open(char *file_mode)
 {
     FILE *fp;
@@ -28,22 +31,106 @@ double f_y(double x)
     return sqrt((x-1)*(x-1)*(x-1));
 }
 
+//Function to get the f'(x)
+//xn next value of x ps x+1
+double fd_y(double y, double yn, double x, double xn)
+{
+    return (yn-y)/(xn-x);
+}
+
+//Function to fill the table with x,y,y'
+void fint_x()
+{
+    int i = 0;
+    char buffer[30];
+    double x, xn, y, yn, yd;
+    FILE *fp = file_open("r");
+
+    printf("\n\n");
+    if(fp)
+    {
+        while(fscanf(fp,"%lf", &y) != EOF)
+        {
+            if(i>2)
+            {
+                x = get_x(i-2);
+                y = f_y(x);
+                t_vals[0][i-3] = x;
+                t_vals[1][i-3] = y;
+                printf("%lf ", y);
+            }
+            i++;
+        }
+        fclose(fp);
+        for(i=0;i<TOTALPOINTS;i++)
+        {
+            if(i<TOTALPOINTS-1)
+            {
+                x = t_vals[0][i];
+                y = t_vals[1][i];
+                xn = t_vals[0][i+1];
+                yn = t_vals[1][i+1];
+                yd = fd_y(y, yn, x, xn);
+                t_vals[2][i] = yd;
+            }
+
+        }
+    }
+    else
+    {
+        printf("Error while opening file %s for read!!!\n", POINTS_FILENAME);
+        exit(1);
+    }
+    return;
+}
+
+void t_print()
+{
+    int i;
+    printf("\n\n");
+    for(i=0;i<TOTALPOINTS;i++)
+    {
+        if(i<TOTALPOINTS-1)
+        {
+            printf("x=%lf y=%lf y'=%lf\n",t_vals[0][i], t_vals[1][i], t_vals[2][i]);
+        }
+
+    }
+
+}
+
+double f_integral()
+{
+    int i;
+    double ret = 0.00;
+    for(i=0;i<(TOTALPOINTS-1);i++)
+    {
+        ret = ret + t_vals[2][i];
+
+    }
+    return ret;
+}
+
+//1<=pos<=TOTALPOINTS
+int get_x(int pos)
+{
+    return (POINT_STARTX + (pos*POINT_STEP));
+}
+
 void file_fill()
 {
     int i;
-    float x, y;
+    double y;
     FILE *fp = file_open("w");
     if(fp)
     {
         fprintf(fp, "%d\n",TOTALPOINTS);
         fprintf(fp, "%d\n",POINT_STARTX);
         fprintf(fp, "%.2f\n",POINT_STEP);
-        x = POINT_STARTX;
-        for(i=0;i<TOTALPOINTS;i++)
+        for(i=1;i<=TOTALPOINTS;i++)
         {
-            x = x + POINT_STEP;
-            y = f_y(x);
-            fprintf(fp, "%.2f\n",y);
+            y = f_y(get_x(i));
+            fprintf(fp, "\n%.2f",y);
         }
         fclose(fp);
     }
@@ -86,6 +173,8 @@ int main()
     file_fill();
     printf("File %s was filled successfully!!!\n",POINTS_FILENAME);
     file_print_contents();
-
+    fint_x();
+    t_print();
+    printf("The integral is : %lf\n\n", f_integral());
     return 0;
 }
