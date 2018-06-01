498d3d0bb6fb997b758a60555ddcb97f4ad16fb0
Panagiotis Pantelakis <panagiotis.pantelakis@gmail.com>
2018-06-02 01:03:20 +0300

    Project PanagiotisPantelakis_lab4.2 started with implementation of algorithm of wikipedia

---
+++ lab4.2/PanagiotisPantelakis_lab4.2/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max_size_base10(char *num1, char *num2)
+{
+    int ret = strlen(num2);
+    if(strlen(num1)>strlen(num2))
+    {
+        ret = strlen(num1);
+    }
+    ret--;
+    return ret;
+}
+
+void split_at(char *num, int pos, double *high, double *low)
+{
+    char *to_high = (char*) malloc(pos);
+    char *to_low = (char*) malloc(strlen(num)-pos);
+    strncpy(to_high, num, pos);
+    strncpy(to_low, num+pos, (strlen(num)-pos));
+    *high = atof(to_high);
+    *low = atof(to_low);
+    return;
+}
+
+double power_ten(int pow)
+{
+    double ret = 1;
+    while(pow>0)
+    {
+        ret = ret*10;
+    }
+    return ret;
+}
+
+double karatsuba(char *num1, char *num2)
+{
+    double ret, high1, low1, high2, low2, z0, z1, z2;
+    char *str1,*str2;
+    int m, m2;
+    if (atof(num1) < 10 || atof(num2) < 10)
+    {
+        ret = atof(num1)*atof(num2);
+    }
+    else
+    {
+        m = max_size_base10(num1,num2);
+        m2 = m/2;
+        split_at(num1,m2,&high1,&low1);
+        split_at(num2,m2,&high2,&low2);
+        sprintf(str1,"%.0lf",low1);
+        sprintf(str2,"%.0lf",low2);
+        z0 = karatsuba(str1, str2);
+        sprintf(str1,"%.0lf",(low1+high1));
+        sprintf(str2,"%.0lf",(low2+high2));
+        z1 = karatsuba(str1, str2);
+        sprintf(str1,"%.0lf",high1);
+        sprintf(str2,"%.0lf",high2);
+        z1 = karatsuba(str1, str2);
+        ret = (z2 * power_ten(2 * m2)) + ((z1 - z2 - z0) * power_ten(m2)) + (z0);
+    }
+
+    return ret;
+}
+
+
+int main()
+{
+    char num1[100], num2[100];
+    double ret;
+    printf("Give me numbers to multiply : ");
+    scanf("%s %s",num1,num2);
+    ret = karatsuba(num1,num2);
+    printf("%.0lf",ret);
+    return 0;
+}
+
+
