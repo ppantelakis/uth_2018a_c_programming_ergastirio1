5d2f1e42a202350622b33c5c18a785fd1ccb3190
Panagiotis Pantelakis <panagiotis.pantelakis@gmail.com>
2018-06-02 01:39:44 +0300

    Project PanagiotisPantelakis_lab4.2 finished with implementation of algorithm as wikipedia pseudo algorithm shows but with no lack. Need investigation and debugging.

--- lab4.2/PanagiotisPantelakis_lab4.2/main.c
+++ lab4.2/PanagiotisPantelakis_lab4.2/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int max_size_base10(char *num1, char *num2)
 {
@@ -14,8 +15,8 @@ int max_size_base10(char *num1, char *num2)
 
 void split_at(char *num, int pos, double *high, double *low)
 {
-    char *to_high = (char*) malloc(pos);
-    char *to_low = (char*) malloc(strlen(num)-pos);
+    char *to_high = (char*) malloc(pos*sizeof(char));
+    char *to_low = (char*) malloc((strlen(num)-pos)*sizeof(char));
     strncpy(to_high, num, pos);
     strncpy(to_low, num+pos, (strlen(num)-pos));
     *high = atof(to_high);
@@ -29,6 +30,7 @@ double power_ten(int pow)
     while(pow>0)
     {
         ret = ret*10;
+        pow--;
     }
     return ret;
 }
@@ -36,7 +38,7 @@ double power_ten(int pow)
 double karatsuba(char *num1, char *num2)
 {
     double ret, high1, low1, high2, low2, z0, z1, z2;
-    char *str1,*str2;
+    char str1[100],str2[100];
     int m, m2;
     if (atof(num1) < 10 || atof(num2) < 10)
     {
@@ -46,11 +48,14 @@ double karatsuba(char *num1, char *num2)
     {
         m = max_size_base10(num1,num2);
         m2 = m/2;
+
         split_at(num1,m2,&high1,&low1);
         split_at(num2,m2,&high2,&low2);
-        sprintf(str1,"%.0lf",low1);
-        sprintf(str2,"%.0lf",low2);
+
+        sprintf(str1,"%lf",low1);
+        sprintf(str2,"%lf",low2);
         z0 = karatsuba(str1, str2);
+        printf("%.0lf",z0);
         sprintf(str1,"%.0lf",(low1+high1));
         sprintf(str2,"%.0lf",(low2+high2));
         z1 = karatsuba(str1, str2);
