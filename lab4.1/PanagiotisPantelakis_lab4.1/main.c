217168b950c5bb7fc32245bdd91a5867dfcbb9d3
Panagiotis Pantelakis <panagiotis.pantelakis@gmail.com>
2018-06-01 19:55:53 +0300

    Created PanagiotisPantelakis_lab4.1 project

---
+++ lab4.1/PanagiotisPantelakis_lab4.1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Maximum no zero elements
+#define M_max 10
+
+//N width,height of table
+#define N_SIZE 60
+
+typedef struct {
+int x;
+int y;
+int val;
+}SparseElement;
+
+SparseElement SparseMatrix[M_max];
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
